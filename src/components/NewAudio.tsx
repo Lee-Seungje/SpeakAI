aa52572b51b4a837a2a984bfe92cd0fe4521bc55
Lee-Seungje <moondgod@gmail.com>
2023-07-28 14:20:27 +0900

    Update animation

--- src/components/NewAudio.tsx
+++ src/components/NewAudio.tsx
@@ -1,6 +1,6 @@
 /** @jsxImportSource @emotion/react */
 
-import React, { useState } from "react";
+import { useState } from "react";
 
 import styled from "@emotion/styled";
 
@@ -11,6 +11,9 @@ import { css } from "@emotion/react";
 const NewAudio = () => {
   const [value, setValue] = useState<string>("");
   const [isJump, setIsJump] = useState<boolean>(false);
+  const [isTurn, setIsTurn] = useState<boolean>(false);
+  const [isRight, setIsRight] = useState<boolean>(false);
+  const [isJong, setIsJong] = useState<boolean>(false);
   window.SpeechRecognition =
     window.SpeechRecognition || window.webkitSpeechRecognition;
 
@@ -23,6 +26,42 @@ const NewAudio = () => {
       .map((results: any) => results[0].transcript)
       .join("");
     setValue(texts);
+
+    switch (texts) {
+      case "점프": {
+        setIsJump(true);
+        setTimeout(() => {
+          setIsJump(false);
+        }, 1000);
+        break;
+      }
+      case "돌아": {
+        setIsTurn(true);
+        setTimeout(() => {
+          setIsTurn(false);
+        }, 1000);
+        break;
+      }
+      case "오른쪽": {
+        setIsRight(true);
+        setTimeout(() => {
+          setIsRight(false);
+        }, 1000);
+        break;
+      }
+      case "최종인": {
+        setIsJong(true);
+        break;
+      }
+      case "채종인": {
+        setIsJong(true);
+        break;
+      }
+      case "이정우": {
+        setIsJong(false);
+        break;
+      }
+    }
   };
 
   return (
@@ -36,15 +75,17 @@ const NewAudio = () => {
       </BTN>
       <Image
         alt="캐릭터"
-        src="/LeeJW.jpg"
+        src={isJong ? "/ChaJI.png" : "/LeeJW.jpg"}
         width={300}
         height={400}
-        css={
-          isJump &&
-          css`
-            position: relative;
-          `
-        }
+        css={css`
+          transition: all ease 1s;
+          position: relative;
+          transform: translateY(${isJump && "-300px"});
+          transform: translateX(${isRight && "300px"});
+          /* transform: translateX(${isRight && "300px"}); */
+          transform: rotate(${isTurn && "180deg"});
+        `}
       />
     </Container>
   );
